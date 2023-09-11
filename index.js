8f3cfbc33be48da5102ed7054a18da7aec63f68f
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-09-11 14:08:55 -0400

    Upgrade dependencies, removing body-parser

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const crypto = require('crypto');
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(fileUpload({createParentPath: true}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
