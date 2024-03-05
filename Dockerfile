045003ffb8b1fa803f5c9832ab81a0913d15390a
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-03-05 16:01:18 -0500

    Fix Dockerfile and allow lint/format defaults

--- Dockerfile
+++ Dockerfile
@@ -19,4 +19,4 @@ RUN npm ci --only=production
 COPY . ./
 
 # Run the web service on container startup.
-CMD ["node", "index.js"]
+CMD ["node", "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-const crypto = require('crypto');
-const express = require('express');
-const fileUpload = require('express-fileupload');
+const crypto = require("crypto");
+const express = require("express");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
-app.use(fileUpload({createParentPath: true}));
-app.use(express.json({limit: '1GB'}));
-app.use(express.urlencoded({extended: true}));
+app.use(fileUpload({ createParentPath: true }));
+app.use(express.json({ limit: "1GB" }));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   try {
-    const algorithm = req.query.algorithm || req.body.algorithm || 'sha256';
+    const algorithm = req.query.algorithm || req.body.algorithm || "sha256";
     const hash = crypto.createHash(algorithm);
     hash.update(req.files.file.data);
-    res.send(hash.digest('hex'));
+    res.send(hash.digest("hex"));
   } catch (e) {
-    res.status(400).send('Bad request. Please check your algorithm and file.');
+    res.status(400).send("Bad request. Please check your algorithm and file.");
   }
 });
 
